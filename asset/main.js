a8948c9fe3c6f5e090dc4f58dbb84f7693cec800
hatake1608 <dominh162002@gmail.com>
2021-09-24 11:27:37 +0700

    update

--- asset/main.js
+++ asset/main.js
@@ -1,13 +1,13 @@
+import storage from "./storage.js";
 const todolist = document.querySelector('.todolist-menu');
 const addTodo = document.querySelector('.add-todo');
 const addInput = document.querySelector('.add-input');
 const close = document.getElementsByClassName('trash-icon');
-const TODOS_STORAGE_KEY = "TODOS-APPS";
 
 const init = {
-    todos:[]
+    todos:storage.get()
 }
-
+console.log(storage.get())
 
 var i = 0;
 for (i = 0; i < close.length; i++) {
@@ -31,41 +31,49 @@ addTodo.addEventListener('click', function() {
     addInput.style.display = 'block';
 })
 
+function render() {
+    const html = init.todos.map(todo =>
+        `
+        <li class="todolist-item">
+        <i class="far fa-trash-alt trash-icon"></i>
+        <i class="fas fa-edit edit-icon"></i>
+                ${todo}
+                </li>
+                `
+                ).join('');
+    todolist.innerHTML = html;
+}
+render();
+
 function newTask() {
     var myInput = addInput.value.trim();
-    const html = `
-        <li class="todolist-item">
-            <i class="far fa-trash-alt trash-icon"></i>
-            ${myInput}
-        </li>
-    `
-    if (myInput === '') {
-        addTodo.style.display = 'block';
-        addInput.style.display = 'none';
-    }
-    else {
-        todolist.innerHTML = html + todolist.innerHTML;
-        localStorage.setItem(TODOS_STORAGE_KEY, addInput.value);
-        init.todos.push(localStorage.getItem(TODOS_STORAGE_KEY));
+    if (myInput != '') {
+        init.todos.unshift(myInput);
     }
     addInput.value = "";
     addTodo.style.display = 'block';
     addInput.style.display = 'none';
-    var i = 0;
-    for (i = 0; i < close.length; i++) {
-    close[i].addEventListener('click', function() {
-        var div = this.parentElement;
-        div.style.display = 'none';
+}
+
+var closes = [...close];
+closes.forEach((clo, index) => {
+    clo.addEventListener('click', function() {
+        console.log(index);
+        // init.todos.splice(index, 1);
+        // storage.set(init.todos);
+        // render();
     })
-}
-}
+})
 
 addInput.addEventListener('keyup', function(event) {
     if (event.keyCode == 13) {
         newTask();
+        storage.set(init.todos);
+        render();
     }else{
         return;
     }
 })
+
 
 console.log(init.todos)
